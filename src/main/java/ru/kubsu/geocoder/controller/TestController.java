865fbaf9b10befae3f26d9a9d3d1436e9ad3abde
kiselev_oleg <kiselevoleg2016@gmail.com>
2023-03-12 10:24:52 +0300

    test spring feign connection to nominatim has been added

--- src/main/java/ru/kubsu/geocoder/controller/TestController.java
+++ src/main/java/ru/kubsu/geocoder/controller/TestController.java
@@ -1,36 +1,45 @@
 package ru.kubsu.geocoder.controller;
 
+import ch.qos.logback.core.status.Status;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.kubsu.geocoder.client.NominatimClient;
+import ru.kubsu.geocoder.dto.RestApiError;
 import ru.kubsu.geocoder.model.Test;
 import ru.kubsu.geocoder.service.TestService;
+
+import java.util.Random;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("tests")
 public class TestController {
-    private final TestService service;
+  private final TestService service;
+  private final NominatimClient nominatimClient;
 
-    @Autowired
-    public TestController(TestService service) {
-        this.service = service;
-    }
+  @Autowired
+  public TestController(TestService service, NominatimClient nominatimClient) {
+    this.service = service;
+    this.nominatimClient = nominatimClient;
+  }
 
-    @GetMapping(value = "/test/id{id}", produces = APPLICATION_JSON_VALUE)
-    public Test getTest(@PathVariable Integer id, @RequestParam String name) {
-        return service.built(id, name);
-    }
+  @GetMapping(value = "/test/id{id}", produces = APPLICATION_JSON_VALUE)
+  public Test getTest(@PathVariable Integer id, @RequestParam String name) {
+    return service.built(id, name);
+  }
 
-    @GetMapping(value = "/addTest", produces = APPLICATION_JSON_VALUE)
-    public void save(@RequestParam String name) {
-        service.save(name);
-    }
+  @GetMapping(value = "/addTest", produces = APPLICATION_JSON_VALUE)
+  public void save(@RequestParam String name) {
+    service.save(name);
+  }
 
-    @GetMapping(value = "/getTest/{name}", produces = APPLICATION_JSON_VALUE)
-    public Test load(@PathVariable String name) {
-        return service.load(name);
-    }
+  @GetMapping(value = "/getTest/{name}", produces = APPLICATION_JSON_VALUE)
+  public Test load(@PathVariable String name) {
+    return service.load(name);
+  }
 
     /*
     * private final TestService service;
@@ -55,4 +64,30 @@ public class TestController {
         return service.load(id);
     }
     * */
+
+
+  @GetMapping(value = "/status", produces = APPLICATION_JSON_VALUE)
+  public String test() {
+    return nominatimClient.search("кубгу","json").get(0).getDisplayName();
+  }
+
+  @GetMapping(value = "/hello", produces = APPLICATION_JSON_VALUE)
+  public ResponseEntity<?> hello() {
+    //react on not found
+    //return null; //body is empty
+    //throw new EntityNotFoundException(); //error 500
+    //return ResponseEntity.ok("test body"); //clearly return type of response
+    //return ResponseEntity.notFound().build(); //clearly return type of response
+    //return ResponseEntity.status(HttpStatus.NOT_FOUND).body("test body"); //clearly return type of response with body
+
+    if((new Random()).nextDouble()>0.5) {
+      return ResponseEntity.status(HttpStatus.OK).body("result");
+    } else {
+      RestApiError error = new RestApiError();
+      error.setError("not found data description");
+      error.setStatus(Status.ERROR);
+      error.setPath("/tests/hello");
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+  }
 }
